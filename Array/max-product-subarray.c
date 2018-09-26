10a6aff7b267094cb9741600792acad1a76c93c1
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2018-09-25 21:52:13 -0700

    Update max-product-subarray.c

--- Array/max-product-subarray.c
+++ Array/max-product-subarray.c
@@ -1,3 +1,18 @@
+/*
+	Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+	Example 1:
+	Input: [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+	Example 2:
+	Input: [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+Youtube Link : https://www.youtube.com/watch?v=vtJvbRlHqTA
+*/
+  
+
 int max3(int a, int b, int c) {
     if (a>=b && a>=c) return a;
     else if (b>=a && b>=c) return b;
