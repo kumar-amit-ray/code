26ba86f8518357a52f69e4986b3fd627ec020530
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2018-09-29 20:43:58 -0700

    Create reverse-word-by-word.c

---
+++ Strings/reverse-word-by-word.c
@@ -0,0 +1,42 @@
+/*
+Given an input string , reverse the string word by word. 
+
+Example:
+Input:  ["t","h","e"," ","s","k","y"," ","i","s"," ","b","l","u","e"]
+Output: ["b","l","u","e"," ","i","s"," ","s","k","y"," ","t","h","e"]
+Note: 
+	• A word is defined as a sequence of non-space characters.
+	• The input string does not contain leading or trailing spaces.
+	• The words are always separated by a single space.
+Follow up: Could you do it in-place without allocating extra space?
+*/
+
+//reverse a string from start to end
+void reverseWord(char *str, int start, int end) {
+    int i;
+    char temp;
+    
+    while(start<end) {
+        temp = *(str+start);
+        *(str+start) = *(str+end);
+        *(str+end) = temp;
+        start++;end--;
+    }
+}
+
+void reverseWords(char* str, int strSize) {
+    int i;
+    int start=0;
+    
+    //reverse the whole word.
+    reverseWord(str, 0, strSize-1);
+    //when a space is encountered, reverse each individual word.
+    for (i=0; i<strSize; i++) {
+        if (str[i] == ' ') {
+            reverseWord(str, start, i-1);
+            start = i+1;
+        }
+    }
+    //last word does not have any following spaces, so take care of it.
+    reverseWord(str, start, strSize-1);
+}
