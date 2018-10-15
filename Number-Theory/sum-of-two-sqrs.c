3a1d0a3d34c46af6a035dbab3a16dfbd5b44b37f
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2018-10-14 22:52:46 -0700

    Create sum-of-two-sqrs.c

---
+++ Number-Theory/sum-of-two-sqrs.c
@@ -0,0 +1,25 @@
+	/*
+  Given a non-negative integer c, your task is to decide whether there're two integers a and b such that a2 + b2 = c.  Sum of Square of Two numbers
+	
+	Example 1:
+	Input: 5
+Output: True
+Explanation: 1 * 1 + 2 * 2 = 5
+	Example 2:
+	Input: 3
+Output: False
+*/
+
+bool judgeSquareSum(int c) {
+    int left, right;
+    int sum; 
+    
+    left = 0; right  = sqrt(c);
+    while (left <= right) {
+        sum = left*left+right*right;
+        if (sum < c) {left++;}
+        else if (sum > c) {right--;}
+        else {return true;}
+    }
+    return false;
+}
