4f27a29e582051a908ff2cf8382d8fdc09b5b91c
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2019-02-27 18:23:24 -0800

    Update longest-consecutive-subsequnce.cpp

--- Array/longest-consecutive-subsequnce.cpp
+++ Array/longest-consecutive-subsequnce.cpp
@@ -1,5 +1,6 @@
 /*
-Given an array of integers, find the length of the longest sub-sequence such that elements in the subsequence are consecutive integers, the consecutive numbers can be in any order.
+Given an array of integers, find the length of the longest sub-sequence such that elements in the subsequence
+are consecutive integers, the consecutive numbers can be in any order.
 Examples
 
 Input: arr[] = {1, 9, 3, 10, 4, 20, 2};
