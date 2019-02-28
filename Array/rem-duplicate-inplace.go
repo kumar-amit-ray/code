4fc8020ca38e6937d466861b567359aa5952f978
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2019-02-27 18:22:25 -0800

    Update rem-duplicate-inplace.go

--- Array/rem-duplicate-inplace.go
+++ Array/rem-duplicate-inplace.go
@@ -6,7 +6,7 @@ you must do this by modifying the input array in-place with O(1) extra memory.
 Youtube Explanation: https://www.youtube.com/watch?v=gf7vdIin0dk
 */
 
-func removeduplicateinline(nums []int) {
+func removeduplicateinline(nums []int) int {
     j := 0
 
     for i:=0; i<len(nums)-1; i++ {
@@ -16,7 +16,8 @@ func removeduplicateinline(nums []int) {
         }
     }
     nums[j] = nums[len(nums)-1]
-    fmt.Println(nums)
-    fmt.Println(j+1)
+    
+    // j+1 is the new length because +1 for the last element
+    return j+1
 }
 
