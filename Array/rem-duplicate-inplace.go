21ce5525b72dbe1360332404096e8a8a98d3a78e
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2019-02-27 18:21:07 -0800

    Update rem-duplicate-inplace.go

--- Array/rem-duplicate-inplace.go
+++ Array/rem-duplicate-inplace.go
@@ -2,30 +2,21 @@
 Given a sorted array nums, remove the duplicates in-place such that duplicates 
 appeared at most twice and return the new length.Do not allocate extra space for another array, 
 you must do this by modifying the input array in-place with O(1) extra memory.
+
+Youtube Explanation: https://www.youtube.com/watch?v=gf7vdIin0dk
 */
-func RemoveDupInplace(nums []int) int {
-  if len(nums) == 1 {
-    return len(nums)
-  }
-  start := 1
-  newlen := 1
-  curr := nums[0]
-  numcount := 1
-  
-  for ;start < len(nums); start++ {
-    if nums[start] == curr {
-      if numcount >= 2 {
-        /* just invalidate the number, not required though */
-        nums[start] = -1
-      } else {
-        numcount++
-        newlen++
-      }
-    } else {
-      curr = nums[start]
-      numcount = 1
-      newlen++
+
+func removeduplicateinline(nums []int) {
+    j := 0
+
+    for i:=0; i<len(nums)-1; i++ {
+        if nums[i] != nums[i+1] {
+          nums[j] = nums[i]
+          j++
+        }
     }
-  }
-  return newlen
+    nums[j] = nums[len(nums)-1]
+    fmt.Println(nums)
+    fmt.Println(j+1)
 }
+
