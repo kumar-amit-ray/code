905ac605a158e8b3d21d44e17259965d09a71669
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2019-07-09 16:23:10 -0700

    Update lcabst.c

--- Tree/lcabst.c
+++ Tree/lcabst.c
@@ -19,3 +19,32 @@ struct node *lca(struct node* root, int n1, int n2)
     return root;
 }
 
+in Python
+=========
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        temp_root = root
+        
+        while temp_root:
+            if temp_root.val < p.val and temp_root.val < q.val:
+                temp_root = temp_root.right
+            elif temp_root.val > p.val and temp_root.val > q.val:
+                temp_root = temp_root.left
+            else:
+                break;
+        
+        return temp_root        
+            
