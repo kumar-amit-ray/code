3958a036414a3c350bf5010c1ccb6e2e6476a627
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2019-07-09 16:03:51 -0700

    Update symmetricTree.c

--- Tree/symmetricTree.c
+++ Tree/symmetricTree.c
@@ -53,3 +53,49 @@ bool isSymmetric(struct TreeNode* root) {
     }
     return false;
 }
+
+In Python
+==========
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is None:
+            return True
+        if root.left is None and root.right is None:
+            return True
+        if root.left is not None and root.right is not None:
+            return self.isSymmetric_internal(root.left, root.right)
+        return False
+    
+    def isSymmetric_internal(self, lt, rt):
+        if lt.val != rt.val:
+            return False
+        if lt.right is None and rt.left is not None:
+            return False
+        if lt.right is not None and rt.left is None:
+            return False
+        if lt.left is None and rt.right is not None:
+            return False
+        if lt.left is not None and rt.right is None:
+            return False
+        if lt.left:
+            result = self.isSymmetric_internal(lt.left, rt.right)
+            if result is False:
+                return False
+        if lt.right:
+            result = self.isSymmetric_internal(lt.right, rt.left)
+            if result is False:
+                return False
+        
+        return True
+            
