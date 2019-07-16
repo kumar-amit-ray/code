83d2fe08758ed1944fdf3ce09da5e8db6ee61c44
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2019-07-16 14:49:29 -0700

    Update partition-k-equal-sum-subset.java

--- Array/partition-k-equal-sum-subset.java
+++ Array/partition-k-equal-sum-subset.java
@@ -39,3 +39,49 @@ class Solution {
         return search(new int[k], row, nums, target);
     }
 }
+
+
+In Python
+=========
+class Solution(object):
+    def canPartitionKSubsets(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        if nums is None or len(nums)==0:
+            return False
+        if k==0:
+            return True
+        if sum(nums)%k != 0:
+            return False
+        target = sum(nums)/k
+
+        row = len(nums)-1
+        while row >=0 and nums[row] == target:
+            row -=1
+            k -=1
+
+        groups = [0]*k
+        return self.canPartitionKSubsets_recursive(groups, row, k, nums, target)
+
+    def canPartitionKSubsets_recursive(self, groups, row, k, nums, target):
+        if row <0:
+            return True
+        
+        nums.sort()
+        v = nums[row]
+        row -=1
+        i=0
+        while i<len(groups):
+            if groups[i]+v <=target:
+                groups[i] +=v
+                if self.canPartitionKSubsets_recursive(groups, row, k, nums, target):
+                    return True
+                groups[i] -=v
+            if groups[i] == 0:
+                break;
+            i +=1
+
+        return False
