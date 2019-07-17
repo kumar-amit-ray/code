b4a0543cead790754c00bc0c50721062177f30fc
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2019-07-16 18:19:04 -0700

    Update two-sum.java

--- Array/two-sum.java
+++ Array/two-sum.java
@@ -26,3 +26,30 @@ class Solution {
         return result;
     }
 }
+
+In Python
+=========
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        result = list()
+        hashmap = dict()
+
+        if nums is None or len(nums) == 0:
+            return result
+        i=0
+        for index, num in enumerate(nums):
+            if (target-num) in hashmap:
+                result.append(hashmap[target - num])
+                result.append(index)
+                break
+                
+            if num not in hashmap:
+                hashmap[num] = index
+            i +=1
+
+        return result
