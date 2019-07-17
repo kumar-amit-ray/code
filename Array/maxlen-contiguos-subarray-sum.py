8cf21bec37977a14bc8de8f597ff6834a368befe
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2019-07-16 22:50:27 -0700

    Update and rename maxlen-contiguos-subarray-sum.java to maxlen-contiguos-subarray-sum.py

--- Array/maxlen-contiguos-subarray-sum.py
+++ Array/maxlen-contiguos-subarray-sum.py
@@ -1,4 +1,4 @@
-/*
+'''
 Given an array of n positive integers and a positive integer s, find the maximum 
 length of a contiguous subarray of which the sum=k.
 Input: arr[] = {1, 4, 20, 3, 10, 5}, sum = 33
@@ -9,31 +9,29 @@ Ouptut: Sum found between indexes 0 to 3
 
 Input: arr[] = {-10, 0, 2, -2, -20, 10}, sum = 20
 Ouptut: No subarray with given sum exists
-*/
+'''
 
-class Solution {
-    public int maxSubArrayLen(int[] nums, int k) {
-        int sum=0;
-        int maxLen=0;
-
-        Map <Integer, Integer> map = new HashMap<>();
+class Solution(object):
+    def maxSubArrayLen(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        sum = 0
+        result = 0
+        map = dict()
+        map[0] = -1  # index = -1 before 0th index. 
+        for index, num in enumerate(nums):
+            sum += num
+            if sum not in map:
+                map[sum] = index
+            if sum-k in map:
+                result = max(result, index-map[sum-k])
         
-        map.put(0, -1);
-        for (int i=0; i<nums.length; i++) {
-            sum += nums[i];
-            if (!map.containsKey(sum)) {
-                map.put(sum, i);
-            }
-            if (map.containsKey(sum-k)) {
-                maxLen = Math.max(maxLen, i - map.get(sum-k));
-            }
-        }
-        return maxLen;
-    }
-}
+        return result
 
-In Python
----------
+
 '''
  Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
 
