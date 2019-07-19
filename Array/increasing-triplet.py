4990c11d159d3a8175a4032a94420b5028e4d4f4
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2019-07-18 18:46:20 -0700

    Update increasing-triplet.py

--- Array/increasing-triplet.py
+++ Array/increasing-triplet.py
@@ -1,4 +1,5 @@
-/*
+'''
+
 	6. Find a sorted subsequence of size 3 in linear time
 Given an array of n integers, find the 3 elements such that a[i] < a[j] < a[k] and i < j < k in 0(n) time. If there are multiple such triplets, then print any one of them.
 Examples:
@@ -8,7 +9,7 @@ Input:  arr[] = {1, 2, 3, 4}
 Output: 1, 2, 3 OR 1, 2, 4 OR 2, 3, 4
 Input:  arr[] = {4, 3, 2, 1}
 Output: No such triplet
-*/
+'''
 
 class Solution(object):
     def increasingTriplet(self, nums):
