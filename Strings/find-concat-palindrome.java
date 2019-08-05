5d8abe47ed0a5a9ff95bc25a3cf0521f3566e0a7
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2019-08-04 18:04:26 -0700

    Update find-concat-palindrome.java

--- Strings/find-concat-palindrome.java
+++ Strings/find-concat-palindrome.java
@@ -1,5 +1,6 @@
   /*
-  Given a list of unique words, find all pairs of distinct indices (i, j) in the given list, so that the concatenation of the two words, i.e. words[i] + words[j] is a palindrome.
+  Given a list of unique words, find all pairs of distinct indices (i, j) in the given list, so that the
+  concatenation of the two words, i.e. words[i] + words[j] is a palindrome.
 	Example 1:
 	Given words = ["bat", "tab", "cat"]
 	Return [[0, 1], [1, 0]]
@@ -17,8 +18,10 @@
 	There are several cases to be considered that isPalindrome(s1 + s2):
 	Case1: If s1 is a blank string, then for any string that is palindrome s2, s1+s2 and s2+s1 are palindrome.
 	Case 2: If s2 is the reversing string of s1, then s1+s2 and s2+s1 are palindrome.
-	Case 3: If s1[0:cut] is palindrome and there exists s2 is the reversing string of s1[cut+1:] , then s2+s1 is palindrome.
-	Case 4: Similiar to case3. If s1[cut+1: ] is palindrome and there exists s2 is the reversing string of s1[0:cut] , then s1+s2 is palindrome.
+	Case 3: If s1[0:cut] is palindrome and there exists s2 is the reversing string of s1[cut+1:] , 
+	then s2+s1 is palindrome.
+	Case 4: Similiar to case3. If s1[cut+1: ] is palindrome and there exists s2 is the reversing 
+	string of s1[0:cut] , then s1+s2 is palindrome.
 	To make the search faster, build a HashMap to store the String-idx pairs.
 	My code:
   */
