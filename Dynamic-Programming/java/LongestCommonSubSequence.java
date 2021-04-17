7a12cc09714b6e4669e0c96d0626cfd5f3a83c44
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2021-04-17 16:06:42 -0700

    Create LongestCommonSubSequence.java

---
+++ Dynamic-Programming/java/LongestCommonSubSequence.java
@@ -0,0 +1,57 @@
+/**
+Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+ 
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.
+Example 2:
+
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+Example 3:
+
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+
+Youtube - https://www.youtube.com/watch?v=ASoaQq66foQ
+@Leetcode - https://leetcode.com/problems/longest-common-subsequence/
+ */
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        if (text1.isEmpty() || text2.isEmpty()) {
+            return 0;
+        }
+        
+        // length of dp is going to be +1 of each string length because we want to take into account
+        // empty string.
+        int[][] dp = new int[text1.length()+1][text2.length()+1];
+        
+        for (int i=0; i<dp.length; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i=0; i<dp[0].length; i++) {
+            dp[0][i] = 0;
+        }
+        for (int i=1; i<dp.length; i++) {
+            for (int j=1; j<dp[0].length; j++) {
+                if (text1.charAt(i-1) == text2.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[text1.length()][text2.length()];
+    }
+}
