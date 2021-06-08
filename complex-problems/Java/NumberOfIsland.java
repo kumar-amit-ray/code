4cf31a83431880281a2b6cb651ad9294cb1212c3
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2021-06-07 20:35:14 -0700

    Create NumberOfIsland.java

---
+++ complex-problems/Java/NumberOfIsland.java
@@ -0,0 +1,68 @@
+/**
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+ 
+
+Example 1:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+Example 2:
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
+
+@Leetcode - https://leetcode.com/problems/number-of-islands/
+@youtube - https://www.youtube.com/watch?v=__98uL6wst8
+ */
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int numsIsland = 0;
+        for (int i=0;i<grid.length; i++) {
+            for (int j=0; j<grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    numsIsland++;
+                    markCells(grid, i, j);
+                }
+            }
+        }
+        return numsIsland;
+    }
+    
+    private void markCells(char[][] grid, int i, int j) {
+        if (i >= grid.length || i < 0 || j >= grid[i].length || j < 0) {
+            return;
+        }
+        if (grid[i][j] == '1') {
+            // mark it as visited
+            grid[i][j] = '2';
+            markCells(grid, i+1, j);
+            markCells(grid, i-1, j);
+            markCells(grid, i, j+1);
+            markCells(grid, i, j-1);
+        }
+    }
+}
