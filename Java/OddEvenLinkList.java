d1a50088a8805da5e4fdcdec4ca035a61d2e862f
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2021-08-09 22:17:23 -0700

    Update OddEvenLinkList.java

--- Java/OddEvenLinkList.java
+++ Java/OddEvenLinkList.java
@@ -9,7 +9,8 @@
  * }
  */
 /**
-    Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+    Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, 
+    and return the reordered list.
 
 The first node is considered odd, and the second node is even, and so on.
 
