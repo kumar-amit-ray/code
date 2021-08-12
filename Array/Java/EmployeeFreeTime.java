8236f000f29d9313b57934604b1852b5c38f0f2d
kumar-amit-ray <43596143+kumar-amit-ray@users.noreply.github.com>
2021-08-11 20:17:11 -0700

    Update EmployeeFreeTime.java

--- Array/Java/EmployeeFreeTime.java
+++ Array/Java/EmployeeFreeTime.java
@@ -5,7 +5,9 @@ Each employee has a list of non-overlapping Intervals, and these intervals are i
 
 Return the list of finite intervals representing common, positive-length free time for all employees, also in sorted order.
 
-(Even though we are representing Intervals in the form [x, y], the objects inside are Intervals, not lists or arrays. For example, schedule[0][0].start = 1, schedule[0][0].end = 2, and schedule[0][0][0] is not defined).  Also, we wouldn't include intervals like [5, 5] in our answer, as they have zero length.
+(Even though we are representing Intervals in the form [x, y], the objects inside are Intervals, not lists or arrays. For example, 
+schedule[0][0].start = 1, schedule[0][0].end = 2, and schedule[0][0][0] is not defined).  Also, we wouldn't include intervals like [5, 5] 
+in our answer, as they have zero length.
 
  
 
